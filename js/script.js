6b0636c2f7cd39584e51723dece43f802e446e75
Antonio <antonio.valverde.balbuena@gmail.com>
2020-09-17 08:38:18 +0200

    Evento de soltar en carrito y añadir lista de la compra

--- js/script.js
+++ js/script.js
@@ -1,46 +1,26 @@
 /* Funcionalidades para la Tienda */
 
 
-function start(e) {
-    e.dataTransfer.effecAllowed = 'move'; // Define el efecto como mover (Es el por defecto)
-    e.dataTransfer.setData("Data", e.target.id); // Coje el elemento que se va a mover
-    e.dataTransfer.setDragImage(e.target, 0, 0); // Define la imagen que se vera al ser arrastrado el elemento y por donde se coje el elemento que se va a mover (el raton aparece en la esquina sup_izq con 0,0)
-    e.target.style.opacity = '0.4'; 
+function comienzoArrastre(event){
+    event.dataTransfer.setData('id_arrastrado', event.target.id);
+    event.dataTransfer.setData('nombreProducto', event.target.dataset.nom);
+    event.dataTransfer.setData('precioProducto', event.target.dataset.pre);
+
 }
 
-function end(e){
-    e.target.style.opacity = ''; // Pone la opacidad del elemento a 1 			
-    e.dataTransfer.clearData("Data");
+function arrastrando(event){
+    //pruebas
 }
 
-function drop(e){
+function permitirSoltar(event){
+    event.preventDefault();
+}
 
-    var elementoArrastrado = e.dataTransfer.getData("Data"); // Elemento arrastrado
-    e.target.appendChild(document.getElementById(elementoArrastrado));
-    e.target.style.border = '';  // Quita el borde
-    tamContX = $('#'+e.target.id).width();
-    tamContY = $('#'+e.target.id).height();
-
-    tamElemX = $('#'+elementoArrastrado).width();
-    tamElemY = $('#'+elementoArrastrado).height();
-
-    posXCont = $('#'+e.target.id).position().left;
-    posYCont = $('#'+e.target.id).position().top;
-
-    // Posicion absoluta del raton
-    x = e.layerX;
-    y = e.layerY;
-
-    // Si parte del elemento que se quiere mover se queda fuera se cambia las coordenadas para que no sea asi
-    if (posXCont + tamContX <= x + tamElemX){
-        x = posXCont + tamContX - tamElemX;
-    }
-
-    if (posYCont + tamContY <= y + tamElemY){
-        y = posYCont + tamContY - tamElemY;
-    }
-
-    document.getElementById(elementoArrastrado).style.position = "absolute";
-    document.getElementById(elementoArrastrado).style.left = x + "px";
-    document.getElementById(elementoArrastrado).style.top = y + "px";
+function alSoltar(objetoContainer, event){
+    var idArrastrado = event.dataTransfer.getData('id_arrastrado'); 
+    var nombreArrastrado = event.dataTransfer.getData('nombreProducto');
+    var precioArrastrado = event.dataTransfer.getData('precioProducto');
+    //objetoContainer.innerHTML = document.getElementById(idArrastrado).outerHTML;
+    var laLista = document.getElementById('listaCompra');
+    laLista.innerHTML += '<li>' + nombreArrastrado + '------' + precioArrastrado + ' €</li>';
 }
